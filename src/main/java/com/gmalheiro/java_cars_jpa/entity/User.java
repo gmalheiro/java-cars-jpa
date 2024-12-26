c182a56bb1063541da254131421941b6c2118a2c
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-12-26 12:34:13 -0300

    refactor: update User class with annotations and fields

--- src/main/java/com/gmalheiro/java_cars_jpa/entity/User.java
+++ src/main/java/com/gmalheiro/java_cars_jpa/entity/User.java
@@ -1,4 +1,33 @@
 package com.gmalheiro.java_cars_jpa.entity;
 
+import com.gmalheiro.java_cars_jpa.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "tb_user")
+@AllArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Enumerated(value = EnumType.STRING)
+    private Role role;
+
+    private String email;
+
+    @OneToMany
+    private List<Car> cars = new ArrayList<Car>();
+
+    @OneToMany
+    private List<Offer> offers = new ArrayList<Offer>();
 }

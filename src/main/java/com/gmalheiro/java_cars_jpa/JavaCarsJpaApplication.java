78217c254d3079d5435ac5496131fae4de0de08a
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-12-26 10:42:51 -0300

    feat: add spring project

---
+++ src/main/java/com/gmalheiro/java_cars_jpa/JavaCarsJpaApplication.java
@@ -0,0 +1,13 @@
+package com.gmalheiro.java_cars_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCarsJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCarsJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gmalheiro/java_cars_jpa/JavaCarsJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmalheiro.java_cars_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCarsJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

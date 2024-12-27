a9e7f136722076f82afe664687c97c1b698307be
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-12-27 11:39:16 -0300

    feat: add Address entity

--- src/main/java/com/gmalheiro/java_cars_jpa/entity/Address.java
+++ src/main/java/com/gmalheiro/java_cars_jpa/entity/Address.java
@@ -1,4 +1,26 @@
 package com.gmalheiro.java_cars_jpa.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Entity(name = "tb_address")
+@Data
+@AllArgsConstructor
 public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String street;
+
+    private String city;
+
+    private String state;
+
+    private String zipCode;
 }

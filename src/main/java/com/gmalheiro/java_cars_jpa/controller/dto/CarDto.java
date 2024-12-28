d9fa53c24f9073135b7db73673f853dcf22d0e7c
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-12-28 02:27:28 -0300

    feat: add CarDto

---
+++ src/main/java/com/gmalheiro/java_cars_jpa/controller/dto/CarDto.java
@@ -0,0 +1,9 @@
+package com.gmalheiro.java_cars_jpa.controller.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record CarDto(@NotBlank String model,
+                     @NotBlank String brand,
+                     @NotBlank int year,
+                     @NotBlank double price) {
+}

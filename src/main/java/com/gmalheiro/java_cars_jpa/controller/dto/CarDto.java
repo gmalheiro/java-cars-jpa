200b169bc7a4d47c689fcb92a5e79efea2c35319
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2025-01-15 23:05:41 -0300

    feat: add toEntity method in CarDto

--- src/main/java/com/gmalheiro/java_cars_jpa/controller/dto/CarDto.java
+++ src/main/java/com/gmalheiro/java_cars_jpa/controller/dto/CarDto.java
@@ -1,8 +1,13 @@
 package com.gmalheiro.java_cars_jpa.controller.dto;
 
+import com.gmalheiro.java_cars_jpa.entity.Car;
 import jakarta.validation.constraints.NotBlank;
 
 public record CarDto(@NotBlank String model,
                      @NotBlank String brand,
-                     @NotBlank int year) {
+                     @NotBlank int year,
+                     @NotBlank double price) {
+    public Car toEntity(CarDto dto) {
+        return new Car.CarBuilder(dto.model, dto.brand, dto.year, dto.price).build();
+    }
 }

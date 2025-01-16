8c7e9ba2c9afe7cfde4f7bbf442b5ff7cf6b9594
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2025-01-16 12:57:17 -0300

    feat: update UserDto to include cars and enhance toEntity method

--- src/main/java/com/gmalheiro/java_cars_jpa/controller/dto/UserDto.java
+++ src/main/java/com/gmalheiro/java_cars_jpa/controller/dto/UserDto.java
@@ -1,15 +1,26 @@
 package com.gmalheiro.java_cars_jpa.controller.dto;
 
 import com.gmalheiro.java_cars_jpa.entity.Address;
+import com.gmalheiro.java_cars_jpa.entity.Car;
 import com.gmalheiro.java_cars_jpa.entity.User;
 import com.gmalheiro.java_cars_jpa.enums.Role;
 import jakarta.validation.constraints.NotBlank;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public record UserDto(@NotBlank String name,
                       @NotBlank Role role,
                       @NotBlank String email,
-                      @NotBlank AddressDto address) {
-    public User toEntity(String name,Role role, String email,AddressDto address) {
-        return new User(name,role,email,address.toEntity());
+                      @NotBlank AddressDto address,
+                      @NotBlank List<CarDto> cars) {
+    public User toEntity(UserDto dto) {
+        Address addressEntity = address.toEntity(address);
+        List<Car> carsEntityList = new ArrayList<>();
+        cars.forEach(c -> carsEntityList.add(c.toEntity(c)));
+        return new User.UserBuilder(name,email,role)
+                .setAddress(addressEntity)
+                .setCars(carsEntityList)
+                .build();
     }
 }
